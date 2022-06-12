babb2127136cfa9a916549aa34f3a635a4a7d776
Evgenii <evgeniisemenovykh@outlook.com>
2022-06-12 21:46:07 +0400

    15

---
+++ 15.php
@@ -0,0 +1,9 @@
+<?php
+
+// BEGIN 
+print_r('Winter' . ' came' . ' for' . ' the' . ' House' . ' of' . ' Frey.');
+// END
+
+/*Выведите на экран
+Winter came for the House of Frey.
+используя конкатенацию слов.*/
