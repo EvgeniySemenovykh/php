da9039d631736a1eb617b376eedf645ef94430b7
Evgenii <evgeniisemenovykh@outlook.com>
2022-06-12 21:23:11 +0400

    13

---
+++ 13.php
@@ -0,0 +1,9 @@
+<?php
+
+// BEGIN
+print_r('"Khal Drogo\'s favorite word is "athjahakar""');
+// END
+
+/*Напишите программу, которая выведет на экран:
+"Khal Drogo's favorite word is "athjahakar""
+Программа должна вывести на экран эту фразу в точности.*/
