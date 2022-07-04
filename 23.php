ad85cbf072333730edfd8db687603f2fd2148476
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-04 14:12:27 +0400

    23

---
+++ 23.php
@@ -0,0 +1,13 @@
+<?php
+
+$family = 'Targaryen';
+
+// BEGIN 
+$pet = 'Dragon';
+// END
+
+print_r($family);
+print_r(' and ');
+print_r($pet);
+
+//Найдите в программе необъявленную переменную и объявите её, присвоив ей значение 'Dragon'.
