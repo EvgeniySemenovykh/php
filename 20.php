6a0c55bc8ead32282a0a5851d664fa21a913b5b3
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-04 13:46:01 +0400

    20

---
+++ 20.php
@@ -0,0 +1,8 @@
+<?php
+
+// BEGIN (write your solution here)
+$motto = 'What Is Dead May Never Die!';
+print_r($motto);
+// END
+
+//Создайте переменную с именем $motto и содержимым What Is Dead May Never Die!. Распечайте содержимое переменной.
