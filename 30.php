134520d91969d39c866c96ef620e95210b4829cd
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-05 19:38:50 +0400

    30

---
+++ 30.php
@@ -0,0 +1,9 @@
+<?php
+
+$stark = 'Arya';
+
+// BEGIN
+print_r("Do you want to eat, {$stark}?");
+// END
+
+//Выведите на экран строку Do you want to eat, <name>?. Где вместо <name> должна использоваться переменная $stark.
