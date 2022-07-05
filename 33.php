1f86fb8835d31e0d46a5ddd08f21e2915656b955
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-05 20:16:18 +0400

    33

---
+++ 33.php
@@ -0,0 +1,13 @@
+<?php
+
+$company1 = 'Apple';
+$company2 = 'Samsung';
+
+// BEGIN
+$company1D= strlen($company1);
+$company2D = strlen($company2);
+
+print_r($company1D + $company2D);
+// END
+
+//В коде программы определены две переменные, содержащие имена компаний. Посчитайте их общую длину в символах и выведите ее на экран.
