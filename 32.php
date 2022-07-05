3c0e7c7bfd285abb821722ee98f5e80e0683d017
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-05 19:55:38 +0400

    32

---
+++ 32.php
@@ -0,0 +1,12 @@
+<?php
+
+// BEGIN (write your solution here)
+$str = <<<EOD
+Lannister, Targaryen, Baratheon, Stark, Tyrell... they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins, crushing those on the ground.
+EOD;
+// END
+
+print_r($str);
+
+//Запишите в переменную $str текст, который приведен ниже. Используйте heredoc-синтаксис.
