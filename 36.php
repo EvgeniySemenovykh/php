46580de15638041c67f8c93ad37934ed9c9de1e9
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-05 21:42:50 +0400

    36

---
+++ 36.php
@@ -0,0 +1,10 @@
+<?php
+
+$text = 'Never forget what you are, for surely the world will not';
+
+// BEGIN (write your solution here)
+print_r("First: {$text[0]}\nLast: {$text[-1]}");
+// END
+
+/*Выведите на экран первую и последнюю буквы предложения, записанного в переменную text
+Постарайтесь создать только одну переменную, в которую сразу запишется нужный текст перед печатью на экран.*/
