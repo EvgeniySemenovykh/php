0d1045a37edde5140fb4ef64c3b43700593a89f2
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-06 16:12:41 +0400

    41

---
+++ 41.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HexletBasics\DefineFunctions\ReturnInstruction;
+
+// BEGIN (write your solution here)
+function sayHurrayThreeTimes()
+{
+$hurry = 'hurray! hurray! hurray!';
+return $hurry;
+}
+// END
+
+//Реализуйте функцию sayHurrayThreeTimes(), которая возвращает строку 'hurray! hurray! hurray!'.
