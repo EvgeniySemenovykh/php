a6f0e504cc24349fa9eb38213921c7324c7df2f7
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-06 15:58:40 +0400

    40

---
+++ 40.php
@@ -0,0 +1,15 @@
+<?php
+
+// Неймспейсы изучаются на Хекслете
+// Здесь они нужны по техническим причинам
+// Просто не обращайте внимания, они не влияют на разработку
+namespace HexletBasics\DefineFunctions\Define;
+
+// BEGIN
+function printMotto()
+{
+    print_r("Winter is coming");
+}
+// END
+
+//Реализуйте функцию с именем printMotto(), которая выведет на экран фразу Winter is coming.
