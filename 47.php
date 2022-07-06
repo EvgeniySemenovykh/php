21e256917b112f36806f625be4e04b6d83dc273e
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-06 21:39:34 +0400

    47

---
+++ 47.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HexletBasics\Logic\CombineExpressions;
+
+// BEGIN (write your solution here)
+function isInternationalPhone($phone)
+{
+    return $phone[0] === '+';
+
+}
+
+// END
+
+/*Реализуйте функцию isInternationalPhone(), которая проверяет формат указанного телефона. 
+Если телефон начинается с +, значит это международный формат.*/
