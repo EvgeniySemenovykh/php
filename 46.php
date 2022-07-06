ad42fdc31d2333740de137f1b3453310a6fd8c46
Evgenii <evgeniisemenovykh@outlook.com>
2022-07-06 20:36:03 +0400

    46

---
+++ 46.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HexletBasics\Logic\Predicates;
+
+// BEGIN 
+function isMister($type)
+{
+    return $type === 'Mister';
+}
+// END
+
+//Напишите функцию isMister(), которая принимает строку и проверяет, является ли она словом 'Mister'.
